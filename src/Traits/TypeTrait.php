812c75f668e4f78477f8c5c2bf2050f4437b70ba
vitor-hugo <vitor83dev@gmail.com>
2024-06-18 13:51:04 -0300

    init: type trait

---
+++ src/Traits/TypeTrait.php
@@ -0,0 +1,77 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\PropertyValidator\Traits;
+
+trait TypeTrait
+{
+    /**
+     * Returns the type of a value
+     * @param mixed $value
+     * @return string "array", "boolean", "float", "integer", "null", "object", "string" or "unknown"
+     */
+    protected function getType(mixed $value): string
+    {
+        $type = gettype($value);
+        return $this->normalizeTypeName($type);
+    }
+
+
+    /**
+     * Returns the type of a property, if is not setted returns "mixed"
+     * @param \ReflectionProperty $property
+     * @return string "array", "boolean", "float", "integer", "mixed", "object", "string" or "unknown"
+     */
+    protected function getPropertyType(\ReflectionProperty $property): string
+    {
+        $propType = $property->getType();
+
+        if ($propType === null) {
+            return "mixed";
+        }
+
+        if (method_exists($propType, "getName")) {
+            $type = $propType->getName();
+        } else {
+            $type = is_string($propType) ? $propType : "unknown";
+        }
+
+        return $this->normalizeTypeName($type);
+    }
+
+
+    protected function normalizeTypeName(?string $type): string
+    {
+        $type = strtolower($type);
+
+        $types = [
+            "array",
+            "boolean",
+            "double",
+            "float",
+            "int",
+            "integer",
+            "mixed",
+            "null",
+            "object",
+            "string",
+        ];
+
+        if (!in_array($type, $types)) {
+            return "unknown";
+        }
+
+        // According to PHP documentation of gettype() function:
+        // "For historical reasons 'double' is returned in case of a float, and not simply 'float'."
+        // But when defining a type you can't use 'double' as a type.
+        if ($type == "double") {
+            return "float";
+        }
+
+        // The PHP gettype() function returns "integer" when the variable type is "int"
+        if ($type == "integer") {
+            return "int";
+        }
+
+        return $type;
+    }
+}
