fde6b0b800507f609e2ce85dd36eba7bce6345b9
vitor-hugo <vitor83dev@gmail.com>
2024-06-19 11:23:43 -0300

    init: IsString validator

---
+++ src/Attributes/Validators/TypeCheckers/IsString.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\PropertyValidator\Attributes\Validators\TypeCheckers;
+
+use Attribute;
+use Torugo\PropertyValidator\Abstract\Validator;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class IsString extends Validator
+{
+    public function validation(mixed $value): void
+    {
+        $type = $this->getType($value);
+
+        if ($type !== 'string') {
+            $this->throwValidationException("Property '{$this->propertyName}' must receive a string, $type received.");
+        }
+    }
+}
