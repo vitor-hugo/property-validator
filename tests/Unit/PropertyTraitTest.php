2e5ae576785e0a28b6b39ae9649f94e00aa76f83
vitor-hugo <vitor83dev@gmail.com>
2024-06-19 07:57:46 -0300

    init: PropertyTrait tests

---
+++ tests/Unit/PropertyTraitTest.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+use Torugo\PropertyValidator\Attributes\Validators\Common\IsOptional;
+use Torugo\PropertyValidator\Traits\PropertyTrait;
+
+#[Group("Unit")]
+#[Group("Traits")]
+#[TestDox("PropertyTrait tests")]
+class PropertyTraitTest extends TestCase
+{
+    use PropertyTrait;
+
+    private string $string = "My Name";
+    private ?int $integer = 40;
+    private mixed $mixed = ['array'];
+
+    #[TestDox("Should throw Exception when the property was not initialized")]
+    public function testShouldThrowExceptionWhenNotInitialized()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("The property must be initialized before usage.");
+        $this->getValue();
+    }
+
+
+    #[TestDox("Method getIsInitialized() should return FALSE when the property was not initialized")]
+    public function testGetIsInitializedShouldReturnFalse()
+    {
+        $this->assertFalse($this->getIsInitialized());
+    }
+
+
+    #[TestDox("Method getIsInitialized() should return TRUE when the property is initialized")]
+    public function testPropertyInitialization()
+    {
+        $property = new \ReflectionProperty($this, "string");
+        $this->initProperty($property, $this);
+        $this->assertTrue($this->getIsInitialized());
+    }
+
+
+    #[TestDox("Method getValue() should return the property's value")]
+    public function testGetValueShouldReturnPropertyValue()
+    {
+        $property = new \ReflectionProperty($this, "string");
+        $this->initProperty($property, $this);
+        $this->assertEquals("My Name", $this->getValue());
+    }
+
+
+    #[TestDox("Method setValue() should update the property's value")]
+    public function testSetValueShouldUpdatePropertyValue()
+    {
+        $property = new \ReflectionProperty($this, "string");
+        $this->initProperty($property, $this);
+        $this->setValue("My Full Name");
+        $this->assertEquals("My Full Name", $this->getValue());
+    }
+
+
+    #[TestDox("Method isNullable() should return if a property can be NULL")]
+    public function testIsNullableMethod()
+    {
+        $property = new \ReflectionProperty($this, "string");
+        $this->initProperty($property, $this);
+        $this->assertFalse($this->isNullable());
+
+        $property = new \ReflectionProperty($this, "integer");
+        $this->initProperty($property, $this);
+        $this->assertTrue($this->isNullable());
+
+        $property = new \ReflectionProperty($this, "mixed");
+        $this->initProperty($property, $this);
+        $this->assertTrue($this->isNullable());
+    }
+
+
+    #[TestDox("Method IsOptional() should return if a property is defined as optional")]
+    public function testIsOptionalMethod()
+    {
+        $property = new \ReflectionProperty($this, "string");
+        $this->initProperty($property, $this);
+        $this->assertFalse($this->isOptional());
+
+        $property = new \ReflectionProperty($this, "integer");
+        $this->initProperty($property, $this);
+        $this->assertTrue($this->isOptional());
+    }
+
+
+    #[TestDox("Method IsRequired() should return if a property is defined as required")]
+    public function testIsRequiredMethod()
+    {
+        $property = new \ReflectionProperty($this, "string");
+        $this->initProperty($property, $this);
+        $this->assertTrue($this->isRequired());
+
+        $property = new \ReflectionProperty($this, "integer");
+        $this->initProperty($property, $this);
+        $this->assertFalse($this->isRequired());
+
+        $property = new \ReflectionProperty($this, "mixed");
+        $this->initProperty($property, $this);
+        $this->assertFalse($this->isRequired());
+    }
+}
