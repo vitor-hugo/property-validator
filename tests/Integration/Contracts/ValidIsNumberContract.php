427ad1bacb6e379bf98de629fbefa2c3fcc4e727
vitor-hugo <vitor83dev@gmail.com>
2024-06-19 14:52:35 -0300

    init: tests for IsNumber validator

---
+++ tests/Integration/Contracts/ValidIsNumberContract.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Integration\Contracts;
+
+use Tests\Common\BaseValidationTestClass;
+use Torugo\PropertyValidator\Attributes\Validators\TypeCheckers\IsNumber;
+
+class ValidIsNumberContract extends BaseValidationTestClass
+{
+    #[IsNumber()]
+    public int $num1 = 2048;
+
+    #[IsNumber()]
+    public float $num2 = 3.1415;
+
+    #[IsNumber()]
+    public mixed $num3 = 2048;
+
+    #[IsNumber('Not a number!!!')]
+    public mixed $num4 = 9.99;
+}
---
+++ tests/Integration/IsNumbersTest.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Integration;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+use Tests\Integration\Contracts\ValidIsNumberContract;
+use Torugo\PropertyValidator\Exceptions\ValidationException;
+
+#[Group("Validators")]
+#[Group("TypeCheckers")]
+#[Group("IsNumber")]
+#[TestDox("IsNumber Validator")]
+class IsNumbersTest extends TestCase
+{
+    private ValidIsNumberContract $stub;
+
+    public function setUp(): void
+    {
+        $this->stub = new ValidIsNumberContract;
+    }
+
+
+    #[TestDox("Number values should be valid")]
+    public function testShouldBeValid()
+    {
+        $this->assertTrue($this->stub->validate());
+    }
+
+
+    #[TestDox("Should throw ValidationException when value is not a number")]
+    public function testShouldThrowWhenValueIsNotANumber()
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage("Property 'num3' must receive integer or float values, string received.");
+        $this->stub->num3 = "2017";
+        $this->stub->validate();
+    }
+
+
+    #[TestDox("Should throw ValidationException with a custom message")]
+    public function testShouldThroWithCustomMessage()
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage("Not a number!!!");
+        $this->stub->num4 = ["array"];
+        $this->stub->validate();
+    }
+}
