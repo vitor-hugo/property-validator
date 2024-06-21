519796817f8bd40c128c03417068067c735d4547
vitor-hugo <vitor83dev@gmail.com>
2024-06-21 11:44:50 -0300

    init: IsNegative number validator

---
+++ src/Attributes/Validators/Numbers/IsNegative.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\PropertyValidator\Attributes\Validators\Numbers;
+
+use Attribute;
+use Torugo\PropertyValidator\Abstract\Validator;
+
+/**
+ * Validates if property's value is negative (lesser than zero)
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class IsNegative extends Validator
+{
+    public function validation(mixed $value): void
+    {
+        $this->expectPropertyTypeToBe(["int", "float", "mixed"]);
+        $this->validateValueType($value);
+
+        if ($value >= 0) {
+            $this->throwValidationException("Property '{$this->propertyName}' only accepts negative numbers, received {$value}.");
+        }
+    }
+
+
+    private function validateValueType(mixed $value)
+    {
+        $type = $this->getType($value);
+        if ($type !== "int" && $type !== "float") {
+            $this->throwValidationException("Property '{$this->propertyName}' must receive float or int values, received $type.");
+        }
+    }
+}
