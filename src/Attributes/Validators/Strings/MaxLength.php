224bde233a6edea9e1f16caa834cacd9a2a358f4
vitor-hugo <vitor83dev@gmail.com>
2024-06-28 10:36:47 -0300

    init: MaxLength validator

---
+++ src/Attributes/Validators/Strings/MaxLength.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\PropertyValidator\Attributes\Validators\Strings;
+
+use Attribute;
+use Torugo\PropertyValidator\Abstract\Validator;
+use Torugo\PropertyValidator\Exceptions\ValidationException;
+use Torugo\TString\Traits\Validators\TStringMaxLength;
+
+/**
+ * Validates if the length of a string is lesser than or equal to a maximum parameter.
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class MaxLength extends Validator
+{
+    use TStringMaxLength;
+
+    /**
+     * @param int $max Maximum accpetable length. Must be >= 1.
+     * @param string|null $errorMessage Custom error message.
+     */
+    public function __construct(
+        private int $max,
+        string|null $errorMessage = null
+    ) {
+        parent::__construct($errorMessage);
+    }
+
+    public function validation(mixed $value): void
+    {
+        if ($this->max < 1) {
+            throw new ValidationException("The MAX argument on '{$this->propertyName}' must be greater than zero.");
+        }
+
+        $this->expectPropertyTypeToBe(["string", "mixed"]);
+        $this->expectPropertyValueToBe("string");
+
+        if (self::maxLength($value, $this->max) === false) {
+            $len = mb_strlen($value);
+            $this->throwValidationException("The length of '{$this->propertyName}' can be at most {$this->max}, $len received.");
+        }
+    }
+}
