af84e358c7b25b0213bcab243e54d8a481ac55e3
vitor-hugo <vitor83dev@gmail.com>
2024-06-28 20:07:53 -0300

    refact: changing required behavior

--- src/Attributes/Validators/Common/IsRequired.php
+++ src/Attributes/Validators/Common/IsRequired.php
@@ -9,26 +9,16 @@ use Torugo\PropertyValidator\Abstract\Validator;
  * Defines a property as required, so that the value cannot be null or empty.
  *
  * By default, all properties of a class that use any of the attributes in this
- * library are treated as required, so using this attribute is only for defining
- * a custom error message when necessary.
+ * library are treated as not nullable, using this attribute the value can't
+ * be empty as well.
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class IsRequired extends Validator
 {
     public function validation(mixed $value): void
     {
-        $isOptional = $this->property->getAttributes(IsOptional::class);
-
-        if ($isOptional) {
-            $this->throwValidationException("The property '{$this->propertyName}' cannot be REQUIRED and OPTIONAL at the same time.", 1);
-        }
-
-        if ($this->isNullable() && $this->propertyType !== "mixed") {
-            $this->throwValidationException("The property '{$this->propertyName}' cannot be REQUIRED and NULLABLE at the same time.", 2);
-        }
-
         if ($this->isValueEmpty($value)) {
-            $this->throwValidationException("The property '{$this->propertyName}' is required.");
+            $this->throwValidationException("Property '{$this->propertyName}' is required.");
         }
     }
 }
