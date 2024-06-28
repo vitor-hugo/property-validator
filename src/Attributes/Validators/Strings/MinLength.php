d3ef1395af14c1d3889faae3c89fa9957de25e52
vitor-hugo <vitor83dev@gmail.com>
2024-06-28 10:43:18 -0300

    init: MinLength validator

---
+++ src/Attributes/Validators/Strings/MinLength.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\PropertyValidator\Attributes\Validators\Strings;
+
+use Attribute;
+use Torugo\PropertyValidator\Abstract\Validator;
+use Torugo\PropertyValidator\Exceptions\ValidationException;
+use Torugo\TString\Traits\Validators\TStringMinLength;
+
+/**
+ * Validates if the length of a string is greater than or equal to a minimum parameter.
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class MinLength extends Validator
+{
+    use TStringMinLength;
+
+    /**
+     * @param int $min Minimum acceptable length. Must be >= 0.
+     * @param string|null $errorMessage Custom error message.
+     */
+    public function __construct(
+        private int $min,
+        string|null $errorMessage = null
+    ) {
+        parent::__construct($errorMessage);
+    }
+
+
+    public function validation(mixed $value): void
+    {
+        if ($this->min < 0) {
+            throw new ValidationException("The MIN argument on '{$this->propertyName}' must be greater than or equal to zero.");
+        }
+
+        $this->expectPropertyTypeToBe(["string", "mixed"]);
+        $this->expectPropertyValueToBe("string");
+
+        if (self::minLength($value, $this->min) === false) {
+            $len = mb_strlen($value);
+            $this->throwValidationException("The length of '{$this->propertyName}' must be at least {$this->min}, $len received.");
+        }
+    }
+}
