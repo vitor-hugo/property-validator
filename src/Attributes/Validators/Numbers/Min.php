71ea51d2772d6f09bd6aaf11acf53e98888c417b
vitor-hugo <vitor83dev@gmail.com>
2024-07-02 13:47:57 -0300

    refact: rafactoring the validation structure

--- src/Attributes/Validators/Numbers/Min.php
+++ src/Attributes/Validators/Numbers/Min.php
@@ -6,14 +6,14 @@ use Attribute;
 use Torugo\PropertyValidator\Abstract\Validator;
 
 /**
- * Validates if property's value is lesser than a given number
+ * Validates whether a number is greater than or equal to a given number.
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Min extends Validator
 {
     /**
-     * @param int|float $min Minimum acceptable number
-     * @param string|null $errorMessage Message to be displayed in case of validation error.
+     * @param int|float $min Minimum acceptable number.
+     * @param string|null $errorMessage Custom error message.
      */
     public function __construct(private int|float $min, string|null $errorMessage = null)
     {
@@ -24,19 +24,10 @@ class Min extends Validator
     public function validation(mixed $value): void
     {
         $this->expectPropertyTypeToBe(["int", "float", "mixed"]);
-        $this->validateValueType($value);
+        $this->expectValueTypeToBe(["int", "float"]);
 
         if ($value < $this->min) {
             $this->throwValidationException("Property '{$this->propertyName}' only accepts numbers greater than or equal to {$this->min}, received {$value}.");
-        }
-    }
-
-
-    private function validateValueType(mixed $value)
-    {
-        $type = $this->getType($value);
-        if ($type !== "int" && $type != "float") {
-            $this->throwValidationException("Property '{$this->propertyName}' must receive float or int values, received $type.");
         }
     }
 }
