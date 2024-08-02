d4f3a96f1cd0ea496abc53cc0fa460ea406fb994
vitor-hugo <vitor83dev@gmail.com>
2024-08-02 14:55:46 -0300

    update: documentation

--- src/Attributes/Handlers/Setters/SetDateTime.php
+++ src/Attributes/Handlers/Setters/SetDateTime.php
@@ -8,15 +8,18 @@ use DateTimeZone;
 use Torugo\PropertyValidator\Abstract\Handler;
 
 /**
- * Sets the property value as DateTime object or formatted string
+ * Sets the property value as DateTime object or formatted string,
  */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class SetDateTime extends Handler
 {
     /**
      * @param string $datetime DateTime object constructor option (default 'now').
-     * @param string|null $format If provided the value will be setted as a formatted string.
-     * @param \DateTimeZone|null $timezone
+     * @param string|null $format If provided, the value will be setted as a formatted string.
+     * @param \DateTimeZone|null $timezone A DateTimeZone object representing
+     *                                     the timezone of $datetime. If $timezone
+     *                                     is omitted or null, the current
+     *                                     timezone will be used.
      */
     public function __construct(
         private string $datetime = "now",
